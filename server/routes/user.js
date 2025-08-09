389b5bab8e697e89eb0520db1e2d18792a63fd7f
SunnyShekhar-code <sunnyshekhar1729@gmail.com>
2025-08-09 22:59:49 +0530

    added bcrypt

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const router= require("express").Router();
 const User=require("../models/user");
-const bcrypt= require("bcrypt");
+const bcrypt= require("bcrypt.js");
+
 const jwt = require('jsonwebtoken');
 
 
