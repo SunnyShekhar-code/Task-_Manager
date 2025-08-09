42e6702321d01fc79c0c94bbe4605194c22eb1a0
SunnyShekhar-code <sunnyshekhar1729@gmail.com>
2025-08-09 23:14:39 +0530

    updated db connection

--- server/conn.js
+++ server/conn.js
@@ -1,12 +1,14 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const conn= async()=>{
-    try{
-        const response=await mongoose.connect(process.env.MONGO_URI);
-        if(response) console.log("Connected to DB");
-
-    }catch(err){
-        console.log(err.message);
-    } 
-}
-module.exports={conn};
+const conn = async () => {
+  try {
+    const response = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (response) console.log("Connected to DB");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+module.exports = { conn };
